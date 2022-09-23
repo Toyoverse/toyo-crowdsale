725af634ba0b6d239b5e001ce4d6267af7799316
Lucas Lima <lucaslima_150@outlook.com>
2022-09-23 17:00:37 -0300

    window eth error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,11 +32,21 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     async function checkAccounts() {
-      const resp = await window?.ethereum?.request({ method: 'eth_accounts' });
+      console.log(window.ethereum?.request);
 
-      if (resp.length > 0) {
-        setAccountConnected(true);
-        toast('Wallet Connected!', { hideProgressBar: true, autoClose: 3000, type: 'success' });
+      if (window.ethereum?.request) {
+        const resp = await window.ethereum.request({ method: 'eth_accounts' });
+
+        console.log(resp);
+
+        if (resp.length > 0) {
+          setAccountConnected(true);
+          toast('Wallet Connected!', {
+            hideProgressBar: true,
+            autoClose: 3000,
+            type: 'success',
+          });
+        }
       }
     }
 
@@ -46,13 +56,21 @@ const Home: NextPage = () => {
   async function connectMetamask() {
     await connectWithMetamask();
     setAccountConnected(true);
-    toast('Wallet Connected!', { hideProgressBar: true, autoClose: 2000, type: 'success' });
+    toast('Wallet Connected!', {
+      hideProgressBar: true,
+      autoClose: 2000,
+      type: 'success',
+    });
   }
 
   async function disconnectMetamask() {
     await disconnect();
     setAccountConnected(false);
-    toast('Wallet Disconnected!', { hideProgressBar: true, autoClose: 2000, type: 'success' });
+    toast('Wallet Disconnected!', {
+      hideProgressBar: true,
+      autoClose: 2000,
+      type: 'success',
+    });
   }
 
   const year = new Date().getFullYear();
@@ -65,24 +83,42 @@ const Home: NextPage = () => {
       </Head>
       <Section bg="bg-section-one">
         <div className="relative w-128 xl:h-104 h-96">
-          <Image src={ToyoLogo} alt="Toyo logo." className="object-contain" sizes="100vw" layout="fill" />
+          <Image
+            src={ToyoLogo}
+            alt="Toyo logo."
+            className="object-contain"
+            sizes="100vw"
+            layout="fill"
+          />
         </div>
         <div className="relative w-[1200px] h-80 -mt-20">
-          <Image src={XeonLogo} alt="Xeon-1 collection." className="object-contain" sizes="100vw" layout="fill" />
+          <Image
+            src={XeonLogo}
+            alt="Xeon-1 collection."
+            className="object-contain"
+            sizes="100vw"
+            layout="fill"
+          />
         </div>
       </Section>
       <Section bg="bg-main">
         <div className="py-32">
-          <h1 className="font-bold text-center text-7xl text-white font-saira">Minting Steps</h1>
+          <h1 className="font-bold text-center text-7xl text-white font-saira">
+            Minting Steps
+          </h1>
         </div>
         <div className="flex flex-row">
           <Step
             title={'Step 1'}
-            text={'Select Polygon/Matic Network in your Metamask, then click the "LOG IN WITH METAMASK" button.'}
+            text={
+              'Select Polygon/Matic Network in your Metamask, then click the "LOG IN WITH METAMASK" button.'
+            }
           />
           <Step
             title={'Step 2'}
-            text={'To enable the mint buttons, confirm that you are not a robot by clicking the reCAPTCHA checkbox.'}
+            text={
+              'To enable the mint buttons, confirm that you are not a robot by clicking the reCAPTCHA checkbox.'
+            }
           />
           <Step
             title={'Step 3'}
@@ -92,12 +128,17 @@ const Home: NextPage = () => {
           />
           <Step
             title={'Step 4'}
-            text={'To mint the boxes, you must wait 33 seconds cooldown and re-check the reCAPTCHA.'}
+            text={
+              'To mint the boxes, you must wait 33 seconds cooldown and re-check the reCAPTCHA.'
+            }
           />
         </div>
         <div className="flex py-32">
           {accountConnected ? (
-            <Button bg={'metamask-connected'} onClick={() => disconnectMetamask()} />
+            <Button
+              bg={'metamask-connected'}
+              onClick={() => disconnectMetamask()}
+            />
           ) : (
             <Button bg={'metamask-login'} onClick={() => connectMetamask()} />
           )}
@@ -107,21 +148,39 @@ const Home: NextPage = () => {
       <Section bg="bg-main">
         <div className="flex flex-col">
           <div className="relative w-96 h-96">
-            <Image src={XeonBox} layout="fill" alt="Xeon box." objectFit="contain" priority />
+            <Image
+              src={XeonBox}
+              layout="fill"
+              alt="Xeon box."
+              objectFit="contain"
+              priority
+            />
           </div>
           <div className="mt-4">
-            <h1 className="font-bold text-center text-4xl text-white font-saira">Xeon-1 Box</h1>
-            <p className="text-center text-white font-barlow">000 minted / 0000 max supply</p>
+            <h1 className="font-bold text-center text-4xl text-white font-saira">
+              Xeon-1 Box
+            </h1>
+            <p className="text-center text-white font-barlow">
+              000 minted / 0000 max supply
+            </p>
           </div>
           <div className="flex flex-row items-center justify-between mt-4 mx-16">
             <div className="flex items-center ml-4">
               <div className="relative w-7 h-7">
-                <Image src={MaticIcon} layout="fill" alt="Matic icon." objectFit="contain" priority />
+                <Image
+                  src={MaticIcon}
+                  layout="fill"
+                  alt="Matic icon."
+                  objectFit="contain"
+                  priority
+                />
               </div>
               <p className="text-blue-400 text-4xl font-barlow">110</p>
             </div>
             <div className="flex items-center">
-              <p className="text-center text-white text-xl pt-2 font-barlow">-U$158</p>
+              <p className="text-center text-white text-xl pt-2 font-barlow">
+                -U$158
+              </p>
             </div>
           </div>
           <div className="flex flex-row items-center justify-between mt-4 mx-16">
@@ -139,7 +198,13 @@ const Home: NextPage = () => {
           </div>
           <div className="flex flex-row justify-center">
             <button className="relative w-64 h-28" onClick={goToPage}>
-              <Image src={MintNowButton} layout="fill" alt="Mint now button." objectFit="contain" priority />
+              <Image
+                src={MintNowButton}
+                layout="fill"
+                alt="Mint now button."
+                objectFit="contain"
+                priority
+              />
             </button>
           </div>
           <div className="flex flex-col mx-16">
@@ -172,38 +237,62 @@ const Home: NextPage = () => {
       </Section>
       <Section bg="bg-main">
         <div className="flex py-12">
-          <h1 className="font-bold text-center text-7xl text-white font-saira">Xeon-1 / Pier-0 Toyos</h1>
+          <h1 className="font-bold text-center text-7xl text-white font-saira">
+            Xeon-1 / Pier-0 Toyos
+          </h1>
         </div>
         <div className="relative w-full h-[628px]">
-          <Image src={XeonToyoList} layout="fill" alt="Xeon toyo list." objectFit="contain" priority />
+          <Image
+            src={XeonToyoList}
+            layout="fill"
+            alt="Xeon toyo list."
+            objectFit="contain"
+            priority
+          />
         </div>
       </Section>
       <Section bg="bg-main">
         <div className="flex py-32">
-          <h1 className="font-bold text-center text-7xl text-white font-saira">Unboxing Event</h1>
+          <h1 className="font-bold text-center text-7xl text-white font-saira">
+            Unboxing Event
+          </h1>
         </div>
         <div className="flex flex-col">
           <p className="mb-8 text-justified text-white font-barlow">
-            Scientists and engineers have worked on this page as a portal that links our simulation (yes, we live in a
-            simulation!) to the Toyo Parallel Metaverse. As of now, no one knows how to open the boxes yet, since they
-            were sealed using an unknown technology.
+            Scientists and engineers have worked on this page as a portal that
+            links our simulation (yes, we live in a simulation!) to the Toyo
+            Parallel Metaverse. As of now, no one knows how to open the boxes
+            yet, since they were sealed using an unknown technology.
           </p>
           <p className="mb-8 text-justified text-white font-barlow">
-            We put a team of archaeologists, anthropologists, and IT nerds to figure out how to open those boxes and
-            reveal which Toyo is inside each of these. Deadlines were given to the team and they confirmed it'll be in
-            2022, exact date TBA.
+            We put a team of archaeologists, anthropologists, and IT nerds to
+            figure out how to open those boxes and reveal which Toyo is inside
+            each of these. Deadlines were given to the team and they confirmed
+            it'll be in 2022, exact date TBA.
           </p>
           <p className="mb-8 text-justified text-white font-barlow">
-            As soon as they figure it out, we will launch an Unboxing Event and you will finally get to meet your
-            Toyo(s).
+            As soon as they figure it out, we will launch an Unboxing Event and
+            you will finally get to meet your Toyo(s).
           </p>
         </div>
         <div className="flex py-32">
           <button className="relative w-60 h-40 mx-12" onClick={goToPage}>
-            <Image src={LearnMoreButton} layout="fill" alt="Learn more button." objectFit="contain" priority />
+            <Image
+              src={LearnMoreButton}
+              layout="fill"
+              alt="Learn more button."
+              objectFit="contain"
+              priority
+            />
           </button>
           <button className="relative w-80 h-40 mx-12" onClick={goToPage}>
-            <Image src={OpenSeaButton} layout="fill" alt="OpenSea button." objectFit="contain" priority />
+            <Image
+              src={OpenSeaButton}
+              layout="fill"
+              alt="OpenSea button."
+              objectFit="contain"
+              priority
+            />
           </button>
         </div>
       </Section>
@@ -211,14 +300,26 @@ const Home: NextPage = () => {
         <div className="flex flex-row flex-1 items-center justify-between min-w-full">
           <div className="flex flex-row">
             <div className="relative w-40 h-40">
-              <Image src={ToyoLogo2} layout="fill" alt="Second toyo logo." objectFit="contain" priority />
+              <Image
+                src={ToyoLogo2}
+                layout="fill"
+                alt="Second toyo logo."
+                objectFit="contain"
+                priority
+              />
             </div>
             <div className="flex flex-col m-4">
               <a href="#home" className="text-white font-barlow">
                 Home
               </a>
-              <ExternalLink link="https://whitepaper.toyoverse.com/" text="Toyo Whitepaper" />
-              <ExternalLink link="https://www.google.com" text="Xeon-1 Drop Article" />
+              <ExternalLink
+                link="https://whitepaper.toyoverse.com/"
+                text="Toyo Whitepaper"
+              />
+              <ExternalLink
+                link="https://www.google.com"
+                text="Xeon-1 Drop Article"
+              />
               <ExternalLink link="https://www.google.com" text="Our Team" />
             </div>
           </div>
@@ -228,21 +329,41 @@ const Home: NextPage = () => {
               <SocialButton bg="telegram" link="https://t.me/toyoverse" />
               <SocialButton bg="twitter" link="https://twitter.com/ToyoVerse" />
               <SocialButton bg="medium" link="https://medium.com/@toyoverse" />
-              <SocialButton bg="youtube" link="https://www.youtube.com/channel/UCKwCI_rs6JyzmMn6TYNVnsA" />
-              <SocialButton bg="instagram" link="https://www.instagram.com/toyoverse/" />
+              <SocialButton
+                bg="youtube"
+                link="https://www.youtube.com/channel/UCKwCI_rs6JyzmMn6TYNVnsA"
+              />
+              <SocialButton
+                bg="instagram"
+                link="https://www.instagram.com/toyoverse/"
+              />
             </div>
             <div className="flex flex-row mt-12">
               <div className="relative w-28 h-8 mr-4">
-                <Image src={PolygonIcon} layout="fill" alt="Polygon icon." objectFit="contain" priority />
+                <Image
+                  src={PolygonIcon}
+                  layout="fill"
+                  alt="Polygon icon."
+                  objectFit="contain"
+                  priority
+                />
               </div>
               <div className="relative w-28 h-8">
-                <Image src={MetamaskIcon} layout="fill" alt="Metamask icon." objectFit="contain" priority />
+                <Image
+                  src={MetamaskIcon}
+                  layout="fill"
+                  alt="Metamask icon."
+                  objectFit="contain"
+                  priority
+                />
               </div>
             </div>
           </div>
         </div>
         <div className="flex mb-16">
-          <p className="text-white font-barlow relative">Copyright © {year} Lucid Dreams. All Rights Reserved.</p>
+          <p className="text-white font-barlow relative">
+            Copyright © {year} Lucid Dreams. All Rights Reserved.
+          </p>
         </div>
       </footer>
     </div>
